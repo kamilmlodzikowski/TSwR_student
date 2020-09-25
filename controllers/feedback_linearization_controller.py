9f972386a4f6fdf96ff406c2e60866ec4c6082bb
Kamil Młodzikowski <k.mlodzikowski@gmail.com>
2020-09-25 12:19:07 +0200

    flc fixed

--- controllers/feedback_linearization_controller.py
+++ controllers/feedback_linearization_controller.py
@@ -5,7 +5,7 @@ from .controller import Controller
 
 class FeedbackLinearizationController(Controller):
     def __init__(self, Tp):
-        self.model = ManiuplatorModel(Tp, 0.02, 0.05)
+        self.model = ManiuplatorModel(Tp, 0.25, 0.35)
         self.kd = np.diag((1, 1)) * [2, -1]
         self.kp = np.diag((1, 1)) * [-2, 1.5]
 
@@ -14,12 +14,12 @@ class FeedbackLinearizationController(Controller):
         Please implement the feedback linearization using self.model (which you have to implement also),
         robot state x and desired control v.
         """
+        q0_dot = x[2:].reshape(-1, 1)
         q0 = x[:2].reshape(-1, 1)
-        q_dot = x[2:].reshape(-1, 1)
-        q_d = q_d.reshape(-1, 1)
         q_d_dot = q_d_dot.reshape(-1, 1)
+        q_d = q_d.reshape(-1, 1)
 
-        v = self.kd.dot(q_dot - q_d_dot) + self.kp.dot(q0 - q_d) + q_dd_dot
+        v = q_dd_dot + self.kd.dot(q0_dot - q_d_dot) + self.kp.dot(q0 - q_d)
         z = self.model.M(x).dot(v) + self.model.C(x).dot(x[2:].reshape(-1, 1))
 
         return z
